1e679671a9792178c9970cf57594d1488c0e9b7a
Case Scally <caseScally@gmail.com>
2020-07-07 18:08:31 -0700

    song file post method

--- MyMusicApp/MyMusic/Controllers/V1/SongsController.cs
+++ MyMusicApp/MyMusic/Controllers/V1/SongsController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
@@ -62,8 +63,15 @@ namespace MyMusic.Controllers.V1
                     {
                             Song foundSong = new Song
                             {
-                                Id = reader.GetInt32(reader.GetOrdinal("Id")),
-                                Name = reader.GetString(reader.GetOrdinal("Name")),
+                            Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                            Name = reader.GetString(reader.GetOrdinal("Name")),
+                            Url = reader.GetString(reader.GetOrdinal("Url")),
+                            ApplicationUserId = reader.GetString(reader.GetOrdinal("ApplicationUserId")),
+                            Genre = reader.GetString(reader.GetOrdinal("Genre")),
+                            CoverUrl = reader.GetString(reader.GetOrdinal("CoverUrl")),
+                            Description = reader.GetString(reader.GetOrdinal("Description")),
+                            ForSale = reader.GetBoolean(reader.GetOrdinal("ForSale")),
+                            ImageFileName = reader.GetString(reader.GetOrdinal("ImageFileName"))
                             };
                             songs.Add(foundSong);
                         }
@@ -78,9 +86,86 @@ namespace MyMusic.Controllers.V1
 
         // GET: api/Songs/5
         [HttpGet("{id}", Name = "GetSong")]
-        public string Get(int id)
+        public async Task<IActionResult> Get([FromRoute] int id)
         {
-            return "value";
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"SELECT s.Id, s.Name, s.Url, s.ApplicationUserId, s.Genre, s.CoverUrl, s.Description, s.ForSale, s.ImageFileName, a.FirstName, a.LastName, a.StreetAddress, a.ProfilePicturePath, a.ProfileBackgroundPicturePath, a.Description, a.ProfileHeader, a.ActiveUser
+                                        FROM Songs s
+                                        LEFT JOIN AspNetUsers a
+                                        ON c.ApplicationUserId = a.Id
+                                        WHERE c.Id = @id AND c.activeCar = 'true'";
+
+                    cmd.Parameters.Add(new SqlParameter("@id", id));
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    Song individualSong = null;
+
+                    if (reader.Read())
+                    {
+                        individualSong = new Song
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                            Name = reader.GetString(reader.GetOrdinal("Name")),
+                            Url = reader.GetString(reader.GetOrdinal("Url")),
+                            ApplicationUserId = reader.GetString(reader.GetOrdinal("ApplicationUserId")),
+                            Genre = reader.GetString(reader.GetOrdinal("Genre")),
+                            CoverUrl = reader.GetString(reader.GetOrdinal("CoverUrl")),
+                            Description = reader.GetString(reader.GetOrdinal("Description")),
+                            ForSale = reader.GetBoolean(reader.GetOrdinal("ForSale")),
+                            ImageFileName = reader.GetString(reader.GetOrdinal("ImageFileName"))
+                        };
+                        reader.Close();
+
+                        return Ok(individualSong);
+                    }
+                    else
+                    {
+                        return NotFound();
+                    }
+                }
+            }
+        }
+
+                [HttpPost("files")]
+        public async Task<List<string>> PostFile()
+        {
+            var savedFilePaths = new List<string>();
+
+            if (Request.Form.Files.Count > 0)
+            {
+                EnsureUploadDirectoryExists();
+                foreach (IFormFile file in Request.Form.Files)
+                {
+                    string savedFilePath = String.Empty;
+                    if (file != null && file.Length > 0)
+                    {
+                        savedFilePath = _environment.WebRootPath + "\\Upload\\"+ Path.GetFileName(file.FileName);
+                        using (var fileStream = new FileStream(savedFilePath, FileMode.Create))
+                        {
+                            await file.CopyToAsync(fileStream);
+                        }
+                        savedFilePaths.Add(savedFilePath);
+                    }
+                }
+            }
+
+            return savedFilePaths;
+        }
+
+        private static void EnsureUploadDirectoryExists()
+        {
+            if (String.IsNullOrWhiteSpace(_environment.WebRootPath))
+            {
+                _environment.WebRootPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot");
+            }
+            if (!Directory.Exists(_environment.WebRootPath + "\\Upload\\"))
+            {
+                Directory.CreateDirectory(_environment.WebRootPath + "\\Upload\\");
+            }
         }
 
         // POST: api/Songs
@@ -99,6 +184,24 @@ namespace MyMusic.Controllers.V1
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
+        }
+                 private bool SongExists(int id)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                        SELECT Id, Name
+                        FROM Songs
+                        WHERE Id = @id";
+                    cmd.Parameters.Add(new SqlParameter("@id", id));
+
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    return reader.Read();
+                }
+            }
         }
     }
 }
